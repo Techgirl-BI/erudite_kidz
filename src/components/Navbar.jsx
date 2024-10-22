ca37d92a03db419c006b4daefcc214ee1d5a99a0
Techgirl-FE <rachealonimisi31@gmail.com>
2024-10-22 10:37:44 +0100

    m'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <nav>
         <div className="logo">
-        <img src="https://tse4.mm.bing.net/th?id=OIP.YqYJ2CALb-KvMOArtfNacgHaEL&pid=Api&P=0&h=220" width={130} height={80} />
+        <img className="companylogo" src="https://tse4.mm.bing.net/th?id=OIP.YqYJ2CALb-KvMOArtfNacgHaEL&pid=Api&P=0&h=220" width={130} height={80} />
         <h1 className="companyname">The Erudite Scholars School <br/>of Maths & Programming</h1>
         </div>
 
